1ea1cd668e09b12d6ff8835227d84cfe4076be56
Rickmorn <115169098+Rickmorn@users.noreply.github.com>
2022-10-14 00:16:52 +0300

    After first homework

---
+++ portfolio2/src/app/app.component.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+import { Routes } from '@angular/router';
+import { CoursesComponent } from './courses/courses.component';
+import { HobbiesComponent } from './hobbies/hobbies.component';
+import { HomeComponent } from './home/home.component';
+import { WorkComponent } from './work/work.component';
+
+const routes: Routes= [
+  {path: "", component: HomeComponent},
+  {path: "courses", component: CoursesComponent},
+  {path: "hobbies", component: HobbiesComponent},
+  {path: "work", component: WorkComponent},
+  {path: "**", redirectTo: ""},
+]
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'portfolio2';
+}
